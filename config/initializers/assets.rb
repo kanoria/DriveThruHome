6d1b9a13b27938e1e0af5e9196c07b247a518caa
Rishav Kanoria <Rishav@dhcp-ccc-750.eduroam.cornell.edu>
2015-10-18 17:31:12 -0400

    fixing assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( fwslider.css )
+Rails.application.config.assets.precompile += %w( jquery-ui.min.js )
+Rails.application.config.assets.precompile += %w( fwslider.js )
+Rails.application.config.assets.precompile += %w( responsive-nav.js )
